1e6374822cd0e2b395b5a4d872c65501f083079e
Iz-fa <fastol35@gmail.com>
2026-01-16 02:05:10 +0100

    Initial commit

---
+++ Learning/login-form/src/App.jsx
@@ -0,0 +1,50 @@
+import {useState} from 'react'
+import './App.css'
+
+ function LoginForm() {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    function toggleShowPassword() {
+          setShowPassword(!showPassword);
+         
+    }
+  
+  
+    return (
+          <>
+            <div>
+              <input
+                placeholder="Email"
+                className="login-input"
+              />
+            </div>
+            <div>
+              <input
+                placeholder="Password"
+                type={showPassword ? 'text' : 'password'}
+                className="login-input"
+              />
+              <button
+                className="show-button"
+                onClick={toggleShowPassword}
+              >
+                {showPassword ? 'Hide' : 'Show'}
+              </button>
+            </div>
+            <button className="login-button">Login</button>
+            <button className="login-button">Sign up</button>
+          </>
+        );
+      }
+
+      function App() {
+        return (
+          <>
+            <p className="title">Hello, welcome to my website</p>
+            <LoginForm />
+          </>
+        );
+      }
+
+export default App
